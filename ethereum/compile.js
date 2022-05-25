672548c9020df1333b4e3c61248da7e4f2709d96
Ruslan Kotov <rkotov93@gmail.com>
2022-05-25 20:02:25 +0400

    cleanup

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -1,30 +1,3 @@
-// const path = require('path')
-// const fs = require('fs')
-// const solc = require('solc')
-
-// const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol')
-// const source = fs.readFileSync(lotteryPath, 'utf8')
-
-// const input = {
-//   language: 'Solidity',
-//   sources: {
-//     'Lottery.sol': {
-//       content: source,
-//     },
-//   },
-//   settings: {
-//     outputSelection: {
-//       '*': {
-//         '*': ['*'],
-//       },
-//     },
-//   },
-// }
-
-// module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
-//   'Lottery.sol'
-// ].Lottery
-
 const path = require('path')
 const fs = require('fs-extra')
 const solc = require('solc')
@@ -60,4 +33,3 @@ for (contract in compiledContracts) {
     JSON.stringify(compiledContracts[contract])
   )
 }
-
